b302e9c87fe2554eaa6cce1e71cc77f5edd5d408
Juan Pinilla <dark@shamain.com.ar>
2012-03-17 10:44:16 -0500

    Added 404 (perfect work :D), and autocompilation of script.js using script-coffee

--- app.coffee
+++ app.coffee
@@ -2,10 +2,13 @@
 # Module dependencies.
 
 express = require 'express'
+coffee = require 'coffee-script'
+fs = require 'fs'
 stylus = require 'stylus'
 nib = require 'nib'
 routes = require './routes'
 
+port = process.env.PORT || 3000
 app = module.exports = express.createServer()
 
 # Configuration
@@ -17,8 +20,8 @@ app.configure () ->
 	app.use express.methodOverride()
 	app.use express.cookieParser()
 	app.use stylus.middleware {
-		src: __dirname + '/stylus',
-		dest: __dirname + '/public',
+		src: __dirname + '/stylus'
+		dest: __dirname + '/public'
 		compile: (str, path) ->
 			return stylus(str)
 				.set('filename', path)
@@ -26,8 +29,15 @@ app.configure () ->
 				.use(nib())
 				.import('nib')
 	}
-	app.use app.router
 	app.use express.static __dirname + '/public'
+	app.use app.router
+	app.use (req, res, next) ->
+		res.render '404', {
+			layout: false
+			status: 404
+		}
+	app.use (err, req, res, next) ->
+		res.send '500', { status: err.status || 500, error: err }
 
 app.configure 'development', () ->
 	app.use express.errorHandler { dumpExceptions: true, showStack: true }
@@ -35,9 +45,26 @@ app.configure 'development', () ->
 app.configure 'production', () ->
 	app.use express.errorHandler()
 
+app.error (err, req, res, next) ->
+	console.log(err, req, res, next)
+
+# Coffee to JS compilation
+app.get '/js/:file.js', (req, res) ->
+	try
+		res.header 'Content-Type', 'application/x-javascript'
+		cs = fs.readFileSync(__dirname+'/coffee/'+req.params.file+'.coffee', 'ascii')
+		js = coffee.compile cs
+		res.send js
+	catch error
+		res.header 'Content-Type', 'text/html; charset=utf-8'
+		res.render '404', {
+			layout: false
+			status: 404
+		}
+
 # Routes
 
 app.get '/', routes.index
 
-app.listen 3000
+app.listen port
 console.log "Express server listening on port %d in %s mode", app.address().port, app.settings.env
--- app.js
+++ app.js
@@ -1,49 +1,86 @@
+(function() {
+  var app, coffee, express, fs, nib, port, routes, stylus;
 
-/**
- * Module dependencies.
- */
+  express = require('express');
 
-var express = require('express')
-	, stylus = require('stylus')
-	, nib = require('nib')
-	, routes = require('./routes');
+  coffee = require('coffee-script');
 
-var app = module.exports = express.createServer();
+  fs = require('fs');
 
-// Configuration
+  stylus = require('stylus');
 
-app.configure(function(){
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser());
-	app.use(stylus.middleware({ 
-		src: __dirname + '/stylus',
-		dest: __dirname + '/public',
-		compile: function (str, path) {
-			return stylus(str)
-				.set('filename', path)
-				.set('compress', true)
-				.use(nib())
-				.import('nib');
-			}
-	}));	
-	app.use(app.router);
-	app.use(express.static(__dirname + '/public'));
-});
+  nib = require('nib');
 
-app.configure('development', function(){
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
+  routes = require('./routes');
 
-app.configure('production', function(){
-	app.use(express.errorHandler()); 
-});
+  port = process.env.PORT || 3000;
 
-// Routes
+  app = module.exports = express.createServer();
 
-app.get('/', routes.index);
+  app.configure(function() {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(stylus.middleware({
+      src: __dirname + '/stylus',
+      dest: __dirname + '/public',
+      compile: function(str, path) {
+        return stylus(str).set('filename', path).set('compress', true).use(nib())["import"]('nib');
+      }
+    }));
+    app.use(express.static(__dirname + '/public'));
+    app.use(app.router);
+    app.use(function(req, res, next) {
+      return res.render('404', {
+        layout: false,
+        status: 404
+      });
+    });
+    return app.use(function(err, req, res, next) {
+      return res.send('500', {
+        status: err.status || 500,
+        error: err
+      });
+    });
+  });
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  app.configure('development', function() {
+    return app.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+  });
+
+  app.configure('production', function() {
+    return app.use(express.errorHandler());
+  });
+
+  app.error(function(err, req, res, next) {
+    return console.log(err, req, res, next);
+  });
+
+  app.get('/js/:file.js', function(req, res) {
+    var cs, js;
+    try {
+      res.header('Content-Type', 'application/x-javascript');
+      cs = fs.readFileSync(__dirname + '/coffee/' + req.params.file + '.coffee', 'ascii');
+      js = coffee.compile(cs);
+      return res.send(js);
+    } catch (error) {
+      res.header('Content-Type', 'text/html; charset=utf-8');
+      return res.render('404', {
+        layout: false,
+        status: 404
+      });
+    }
+  });
+
+  app.get('/', routes.index);
+
+  app.listen(port);
+
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+}).call(this);
